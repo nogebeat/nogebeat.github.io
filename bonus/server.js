9536339e6cc1467c55ea036ab96bdc96b9faee45
nogebeat <146479712+nogebeat@users.noreply.github.com>
2024-05-03 12:06:50 +0100

    first push to deploy

---
+++ bonus/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'site')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'site', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
