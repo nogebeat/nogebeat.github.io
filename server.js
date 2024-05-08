e188dc4f20dcba5982b0cb244f274f5b70e35e7f
nogebeat <146479712+nogebeat@users.noreply.github.com>
2024-05-08 01:38:27 +0000

    fix: all files in folder site is moved in root

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'site')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'site', 'index.html'));
+    res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
